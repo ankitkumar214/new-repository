d80ed950c659f2d0150644d524dbc6b4e63d9875
Harsh Kumar <harsh2kumar0108@gmail.com>
2023-05-31 16:30:49 +0530

    harsh's commit

---
+++ Example.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Example {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("this harsh program");
+        System.out.println("Enter a Number");
+        a = sc.nextInt();
+        System.out.println("number is:" + a);
+    }
+}
